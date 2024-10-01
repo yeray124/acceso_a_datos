40886dc6b180f9dfb85027b9a058684b6de4108e
yeray124 <yeray0451@gmail.com>
2024-10-01 19:36:55 +0200

    Ejercicio6
    Despues de muchos problemas :)

--- Uno/src/paquete/Ejercicio1.java
+++ Uno/src/paquete/Ejercicio1.java
@@ -5,7 +5,7 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		 
-		File fichero = new File("C:\\Users\\AlumnoT\\Desktop\\acceso_a_datos\\Uno\\uno");
+		File fichero = new File("cine_granada");
 		fichero.mkdir();
 		String[] Semana = { "Lunes","Martes","Miercoles","Jueves","Viernes","Sabado", "Domingo"};
 		fichero.mkdir();
@@ -14,23 +14,22 @@ public class Ejercicio1 {
             File auxiliar = new File("C:\\Users\\AlumnoT\\Desktop\\acceso_a_datos\\Uno\\cine_granada\\" + dia);
 
             auxiliar.mkdir();
-            File destino = new File( "C:\\Users\\AlumnoT\\Desktop\\acceso_a_datos\\Uno\\cine_granada\\" + fichero, dia); 
+            File destino = new File("cine_granada/" + dia);
             
             if(!destino.exists()) {
             	destino.mkdir();
             	 System.out.println("Directorio cine_granada creado correctamente: " + fichero.getAbsolutePath());
             }
-            
             if(!auxiliar.exists()) {
             	auxiliar.mkdir();
             	 System.out.println("Directorio auxiliar " + dia + " creado correctamente: " + auxiliar.getAbsolutePath());
             }
-            
             if (auxiliar.renameTo(destino)) {
-            	System.out.println("Directorio " + dia + " movido correctamente a: " + destino.getAbsolutePath());
+            	System.out.println(destino.getAbsolutePath());
+            	System.out.println(destino.getPath());
             } else {
             	System.out.println("No se pudo mover el directorio" + dia + "a cine_granada");
-            }
+            }   
         }
     }
 }
