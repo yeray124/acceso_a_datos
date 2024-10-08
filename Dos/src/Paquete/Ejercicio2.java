fdf6d32052ccb940fd228f537427b46de579114f
yeray124 <yeray0451@gmail.com>
2024-10-08 17:58:47 +0200

    Ejercicio3
    :)

--- Dos/src/Paquete/Ejercicio2.java
+++ Dos/src/Paquete/Ejercicio2.java
@@ -9,9 +9,25 @@ public class Ejercicio2 {
 
 	public static void main(String[] args)  throws FileNotFoundException {
 		StreamTokenizer recibir = new StreamTokenizer(new FileInputStream("tema2_practica.txt"));
+		 boolean empieza = false;
 		try {
 			while(recibir.nextToken() !=recibir.TT_EOF) {
-				if (recibir.ttype == StreamTokenizer.TT_NUMBER){
+				
+				if (recibir.ttype == '>'){
+					empieza = true;
+				} 
+				
+				if(empieza) {
+					if (recibir.ttype == recibir.TT_WORD) {
+						System.out.print("Contacto: ");
+						String titular = recibir.sval;
+						recibir.nextToken();
+						String apellidos = recibir.sval;
+						System.out.println(titular + " " + apellidos );
+						empieza = false;
+				} 
+				}
+				else if(recibir.ttype == StreamTokenizer.TT_NUMBER){
 					System.out.print("Numero: ");
 					System.out.println((int)recibir.nval);
 				}
@@ -20,5 +36,4 @@ public class Ejercicio2 {
 			e.printStackTrace();
 		}
 	}
-
 }
