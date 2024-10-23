3c916834a8b52fbed9b7da7a9cdddde89cdc0c94
yeray124 <yeray0451@gmail.com>
2024-10-23 20:06:52 +0200

    Ejercicio1
    Primer commit push primer ejercicio

---
+++ practica5/src/main/java/com/example/yeraysantiago/practica5/Practica5Application.java
@@ -0,0 +1,13 @@
+package com.example.yeraysantiago.practica5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica5Application.class, args);
+	}
+
+}
---
+++ practica5/src/test/java/com/example/yeraysantiago/practica5/Practica5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yeraysantiago.practica5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
