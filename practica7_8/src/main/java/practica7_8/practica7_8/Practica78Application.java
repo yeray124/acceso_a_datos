bcaf1b0519588f79631d5e1319c6b06e31c2296a
yeray124 <yeray0451@gmail.com>
2024-11-06 20:54:06 +0100

    siguiente practica

---
+++ practica7_8/src/main/java/practica7_8/practica7_8/Practica78Application.java
@@ -0,0 +1,13 @@
+package practica7_8.practica7_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica78Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica78Application.class, args);
+	}
+
+}
---
+++ practica7_8/src/test/java/practica7_8/practica7_8/Practica78ApplicationTests.java
@@ -0,0 +1,13 @@
+package practica7_8.practica7_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica78ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
