7bacedc7a321eb87a8184fe2a8a5cfa9e61f312a
yeray124 <yeray0451@gmail.com>
2025-01-28 19:54:23 +0100

    Inicio Acceso a datos
    Creacion de Spring Boot con mongoDB

---
+++ Practica12_13_14/src/main/java/com/example/demo/Practica121314Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica121314Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica121314Application.class, args);
+	}
+
+}
