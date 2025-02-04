027b6f7305dc7f79fb8dd1e0d19d50bc417123ec
yeray124 <yeray0451@gmail.com>
2025-02-04 19:38:50 +0100

    Paso 8
    Finalizado

---
+++ Practica12_13_14/src/main/java/com/example/demo/Transaction.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import java.sql.Date;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document public class Transaction {
+	private String Id;
+	private String Descripcion;
+	private double Quantity;
+	private boolean Income;
+	private Date Fecha;
+	
+	
+	public Transaction(String Id, String Descripcion, double Quantity, boolean Income, Date Fecha) {
+		this.Id = Id;
+		this.Descripcion = Descripcion;
+		this.Quantity = Quantity;
+		this.Income = Income;
+		this.Fecha = Fecha;
+
+	}
+	
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getDescripcion() {
+		return Descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+
+	public double getQuantity() {
+		return Quantity;
+	}
+
+	public void setQuantity(double quantity) {
+		this.Quantity = quantity;
+	}
+
+	public boolean isIncome() {
+		return Income;
+	}
+
+	public void setIncome(boolean income) {
+		Income = income;
+	}
+
+	public Date getFecha() {
+		return Fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.Fecha = fecha;
+	}
+}
